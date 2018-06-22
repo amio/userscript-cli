c5325aa207a60be51eefbbaf8fb5e4c66ae93dfe
Amio <amio.cn@gmail.com>
2018-06-22 18:40:25 +0800

    Preserve userscript metablock format

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const fs = require('fs')
 const babel = require('@babel/core')
+const usp = require('userscript-parser')
 
 module.exports = function (sourceFile, targetFile) {
+  const { metablock, content } = usp(fs.readFileSync(sourceFile, 'utf8'))
   const outro = ';exports["default"]()'
-  const { code } = babel.transformFileSync(sourceFile, {
+  const { code } = babel.transform(content, {
     plugins: ['@babel/plugin-transform-modules-commonjs']
   })
 
-  fs.writeFileSync(targetFile, code + outro)
+  const output = [metablock, code, outro].join('\n')
+  fs.writeFileSync(targetFile, output)
 }
